acefb63ed4a76eab4dc516b2bd8ddfcec59a77c2
negar mohamadhasan <nemote@gmail.com>
2016-12-06 15:53:01 +0330

    statistics example

---
+++ statistics.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="http://cdn.oesmith.co.uk/morris-0.4.1.min.js"></script>
+    <meta charset=utf-8 />
+    <title>statistics</title>
+</head>
+<body>
+<div id="area-example" style="width: 800px;height: 400px;margin: auto;"></div>
+</body>
+<script>
+    /*
+     * Play with this code and it'll update in the panel opposite.
+     *
+     * Why not try some of the options above?
+     */
+    Morris.Area({
+        element: 'area-example',
+        data: [
+            { y: '2006', a: 100, b: 90 },
+            { y: '2007', a: 75,  b: 65 },
+            { y: '2008', a: 50,  b: 40 },
+            { y: '2009', a: 75,  b: 65 },
+            { y: '2010', a: 50,  b: 40 },
+            { y: '2011', a: 75,  b: 65 },
+            { y: '2012', a: 100, b: 90 }
+        ],
+        xkey: 'y',
+        ykeys: ['a', 'b'],
+        labels: ['Series A', 'Series B']
+    });
+</script>
+</html>
