589bbdfbfc970d61c3881e1714c7d9ae0f26a515
samanemalmir <saman9452@gmail.com>
2016-12-06 20:09:39 +0330

    add stylesheet

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 <?php
 try {
     $user='root';
-    $pass = '123123';
+    $pass = '9452';
     $dbh = new PDO('mysql:host=localhost;dbname=avita', $user, $pass);
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
